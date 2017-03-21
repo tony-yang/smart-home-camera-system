dff948c11beb35b06534da5ded2d871cd35f6cfe
Tony Yang <tianyuyang658@yahoo.com>
2017-03-21 15:16:32 -0700

    Fix the root path typo

--- cameraweb/app/controllers/home_controller.rb
+++ cameraweb/app/controllers/home_controller.rb
@@ -22,7 +22,7 @@ class HomeController < ApplicationController
       session[:user_id] = authorized_user.id
       session[:username] = authorized_user.username
       flash[:notice] = 'You are now logged in'
-      redirect_to(root)
+      redirect_to(root_path)
     else
       flash.now[:notice] = 'Invalid username/password'
       render('login')
