f847e4a23e8683f7b6b770b487d08bf8e0fe021a
Tony Yang <tianyuyang658@yahoo.com>
2017-03-21 15:04:36 -0700

    Make sure authentication private method is in the controller

--- cameraweb/app/controllers/application_controller.rb
+++ cameraweb/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  def authenticate_user
+    unless session[:user_id]
+      flash[:notice] = 'Please log in'
+      redirect_to(home_login_path)
+  end
 end
--- cameraweb/app/models/application_record.rb
+++ cameraweb/app/models/application_record.rb
@@ -1,11 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  private
-
-  def authenticate_user
-    unless session[:user_id]
-      flash[:notice] = 'Please log in'
-      redirect_to(home_login_path)
-  end
 end
